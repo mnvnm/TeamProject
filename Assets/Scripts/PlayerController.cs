dd23821a74d7b3ede9035140f013a9a01eb59456
DESKTOP-A3GV9VV\202_01 <wlsdud369784@gmail.com>
2025-07-16 13:46:39 +0900

    플레이어 점프 변경 에셋 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,6 @@ public class PlayerController : MonoBehaviour
     private float m_jumpForce = 380;
 
     private bool m_isGrounded = true;
-    private int m_jumpCount = 0;
-    private int m_jumpCapacity = 2;
 
     private Rigidbody2D m_rigid;
     private Animator m_animator;
@@ -27,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         Jump();
+    transform.rotation = Quaternion.identity;
     }
 
     private void Move()
@@ -38,20 +37,18 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && m_jumpCount < m_jumpCapacity)
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && m_isGrounded)
         {
             m_rigid.linearVelocity = new Vector2(m_rigid.linearVelocity.x, 0);
-            m_rigid.AddForce(new Vector2(0, m_jumpForce));
-            m_jumpCount++;
+            m_rigid.AddForce(gameObject.transform.up * m_jumpForce);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.contacts[0].normal.y > 0.99f && !m_isGrounded)
+        if (collision.contacts[0].normal.y > 0.99f || collision.gameObject.CompareTag("Ground"))
         {
             m_isGrounded = true;
-            m_jumpCount = 0;
         }
     }
 
@@ -60,7 +57,6 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             m_isGrounded = false;
-            m_jumpCount = m_jumpCount < 1 ? 1 : m_jumpCount;
         }
     }
 }
