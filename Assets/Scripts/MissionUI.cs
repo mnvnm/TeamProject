869d39a46709a5f06561542cf5106ef6026f6b64
DESKTOP-A3GV9VV\202_01 <wlsdud369784@gmail.com>
2025-07-23 17:37:41 +0900

    게이지에 따른 미션 UI 텍스트 색상 변경

--- Assets/Scripts/MissionUI.cs
+++ Assets/Scripts/MissionUI.cs
@@ -9,9 +9,15 @@ public class MissionUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI subMissionLifeSupportText;
     [SerializeField] TextMeshProUGUI emergencyMissionText;
 
+    Color commonColor;
+    Color warningColor;
+    Color dangerColor;
+
     void Start()
     {
-
+        commonColor = HexColor("#8FFF8D");
+        warningColor = HexColor("#FFDA00");
+        dangerColor = HexColor("#FF462B");
     }
 
     public void Init()
@@ -32,9 +38,9 @@ public class MissionUI : MonoBehaviour
         {
             mainMissionText.text = string.Format("- 통신기 수리를 완료 하여 구조선을 부르세요!");
         }
-        else if (MissionManager.Inst.nonsense.IsSuccess)
+        if (MissionManager.Inst.nonsense.IsSuccess)
         {
-            mainMissionText.text = string.Format("- 구조선이 올 때까지 버티세요!");
+            mainMissionText.text = string.Format("- 구조선이 올 때까지 생존하세요!");
         }
         else
         {
@@ -44,27 +50,44 @@ public class MissionUI : MonoBehaviour
         if (MissionManager.Inst.LifeSupportGauge > 50.0f)
         {
             subMissionLifeSupportText.text = string.Format("- 생명유지장치가 0% 가 되지 않도록 주의하세요!\n0% 가 되면 플레이어가 사망합니다.");
+            subMissionLifeSupportText.color = commonColor;
         }
         if (MissionManager.Inst.LifeSupportGauge <= 50.0f)
         {
             subMissionLifeSupportText.text = string.Format("- !주의! 생명유지장치가 50% 미만입니다.");
+            subMissionLifeSupportText.color = warningColor;
         }
         if (MissionManager.Inst.LifeSupportGauge < 25.0f)
         {
-            subMissionLifeSupportText.text = string.Format("- !위험! 생명유지장치가 25% 미만입니다. 어서 산소를 넣으세요!");
+            subMissionLifeSupportText.text = string.Format("- !위험! 생명유지장치가 25% 미만입니다.\n어서 산소를 넣으세요!");
+            subMissionLifeSupportText.color = dangerColor;
         }
 
         if (MissionManager.Inst.GeneratorGauge > 50.0f)
         {
             subMissionGeneratorText.text = string.Format("- 발전기가 0% 가 되지 않도록 주의하세요!\n0% 가 되면 생명유지장치 수치가 급감합니다.");
+            subMissionGeneratorText.color = commonColor;
         }
         if (MissionManager.Inst.GeneratorGauge <= 50.0f)
         {
             subMissionGeneratorText.text = string.Format("- !주의! 발전기 충전량이 50% 미만입니다.");
+            subMissionGeneratorText.color = warningColor;
         }
         if (MissionManager.Inst.GeneratorGauge < 25.0f)
         {
-            subMissionGeneratorText.text = string.Format("- !위험! 발전기 충전량이 25% 미만입니다. 어서 발전기를 돌리세요!");
+            subMissionGeneratorText.text = string.Format("- !위험! 발전기 충전량이 25% 미만입니다.\n어서 발전기를 돌리세요!");
+            subMissionGeneratorText.color = dangerColor;
         }
+    }
+    public static Color HexColor(string hexCode)
+    {
+        Color color;
+        if (ColorUtility.TryParseHtmlString(hexCode, out color))
+        {
+            return color;
+        }
+
+        Debug.LogError("[UnityExtension::HexColor]invalid hex code - " + hexCode);
+        return Color.white;
     }
 }
